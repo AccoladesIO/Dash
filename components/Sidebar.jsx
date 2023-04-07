84e39041b86d88ffd20b351c06a1623d163970b2
unknown <ononobip@gmail.com>
2023-04-07 18:26:37 +0100

    ref: refractored this code

---
+++ components/Sidebar.jsx
@@ -0,0 +1,69 @@
+import Link from 'next/link'
+import React from 'react'
+import {RxSketchLogo, RxDashboard, RxPerson} from 'react-icons/rx'
+import {HiOutlineShoppingBag} from 'react-icons/hi'
+import {FiSettings} from 'react-icons/fi'
+
+
+// use children props to display the main component of the app
+const Sidebar = ({children}) => {
+  return (
+    <div className='flex '>
+        <div className='fixed w-20 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between'>
+
+            {/* sidebar for dashboard */}
+            <div className='flex flex-col items-center'>
+
+                {/* home redirect link for dashboard */}
+
+                    <Link href='/'>
+                        <div className='bg-purple-800 text-white p-3 rounded-lg inline-block'>
+                            <RxSketchLogo size={20} />
+                        </div>
+                    </Link>
+
+                    {/* span for spacing between home link and other links  */}
+                    
+                    <span className='border-b-[1px] border-gray-200 w-full p-2'></span>
+
+                    {/*  Dashboard*/}
+
+                    <Link href='/'>
+                        <div className='bg-gray-100 cursor-pointer hover:bg-gray-200 my-4 p-3 rounded-lg inline-block'>
+                            <RxDashboard size={20} />
+                        </div>
+                    </Link>
+
+                    {/* Person*/}
+
+                    <Link href='/'>
+                        <div className='bg-gray-100 cursor-pointer hover:bg-gray-200 my-4 p-3 rounded-lg inline-block'>
+                            <RxPerson size={20} />
+                        </div>
+                    </Link>
+
+                    {/*  shopping */}
+
+                    <Link href='/'>
+                        <div className='bg-gray-100 cursor-pointer hover:bg-gray-200 my-4 p-3 rounded-lg inline-block'>
+                            <HiOutlineShoppingBag size={20} />
+                        </div>
+                    </Link>
+
+                    {/*  settings */}
+
+                    <Link href='/'>
+                        <div className='bg-gray-100 cursor-pointer hover:bg-gray-200 my-4 p-3 rounded-lg inline-block'>
+                            <FiSettings size={20} />
+                        </div>
+                    </Link>
+            </div>
+        </div>
+        
+        {/* main app display with the children props */}
+        <main className='ml-20 w-full'>{children}</main>
+    </div>
+  )
+}
+
+export default Sidebar
